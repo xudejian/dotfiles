9ef2ee1bbb050419f37ee1d0cacc598fa82ed0b7
Dejian Xu <xudejian2008@gmail.com>
2013-01-29 16:54:37 +0800

    add $HOME/Library/LaunchAgents/ to restart.sh

---
+++ zsh/restart.zsh
@@ -0,0 +1,34 @@
+launchctl_restart () {
+	SUDO=''
+	owner=`ls -l ${need_restart} | awk '{print $3}'`
+	if [[ $owner = 'root' ]]; then
+		SUDO='sudo'
+	fi
+	$SUDO launchctl unload -w $1
+	$SUDO launchctl load -w $1
+}
+
+restart () {
+	for o in $@
+	do
+		need_restart=()
+		for i in /Library/LaunchDaemons/* $HOME/Library/LaunchAgents/*
+		do
+				regexp="$o.*\.plist$"
+				if [[ $i =~ $regexp ]]; then
+					need_restart+="$i"
+				fi
+		done
+
+		[[ ${#need_restart} > 1 ]] && {
+			echo 'too more restart'
+			print -l ${need_restart}
+			return 0
+		}
+		[[ ${#need_restart} < 1 ]] && {
+			echo 'not found something like ' $o
+			return 0
+		}
+		launchctl_restart ${need_restart}
+	done
+}
