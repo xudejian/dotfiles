6ba02916271d0fdc76e0e205436ccb73f284ce96
Dejian Xu <xudejian2008@gmail.com>
2013-11-20 09:25:56 +0800

    ignore error of vim

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,3 +1,3 @@
 which trash > /dev/null 2>&1 && alias rm=trash
 alias vimyaction='vi /usr/local/etc/privoxy/my.action'
-vim --noplugin -g +qall && alias vi='vim -g --remote-tab-silent'
+vim --noplugin -g +qall > /dev/null 2>&1 && alias vi='vim -g --remote-tab-silent'
