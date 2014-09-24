860379dfce29d56decca2315d53d799f4fce6752
Dejian Xu <xudejian2008@gmail.com>
2014-09-24 13:15:34 +0800

    update git,privoxy

--- git_template/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git config --global alias.ctags '!.git/hooks/ctags'
-git config --global alias.vimodify 'for i in $(git ls-files -m 2>/dev/null);do vi $i;break;done'
-
--- scripts/install/git.sh
+++ scripts/install/git.sh
@@ -2,6 +2,8 @@
 DIR=`pwd`
 [ -f $DIR/gitignore_global ] && git config --global core.excludesfile $DIR/gitignore_global
 
+[ -f $DIR/git_template ] && git config --global init.templatedir ~/src/dotfiles/git_template
+
 git config --global user.email 'xudejian2008@gmail.com'
 git config --global user.name 'Dejian Xu'
 
@@ -11,3 +13,6 @@ git config --global color.branch auto
 git config --global color.interactive auto
 git config --global color.ui auto
 git config --global color.grep auto
+
+git config --global alias.ctags '!.git/hooks/ctags'
+git config --global alias.vimodify 'for i in $(git ls-files -m 2>/dev/null);do vi $i;break;done'
