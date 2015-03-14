e41c332a89a4ddbf93aee56c792e6bebbd8892c2
Dejian Xu <xudejian2008@gmail.com>
2015-03-15 01:13:41 +0800

    git config --global svn.rmdir true

--- scripts/install/git.sh
+++ scripts/install/git.sh
@@ -16,3 +16,5 @@ git config --global color.grep auto
 
 git config --global alias.ctags '!.git/hooks/ctags'
 git config --global alias.vimodify 'for i in $(git ls-files -m 2>/dev/null);do vi $i;break;done'
+
+git config --global svn.rmdir true
