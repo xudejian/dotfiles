635bc5f28383e814bbf2a6aab8b6de64039ba80d
Dejian Xu <xudejian2008@gmail.com>
2015-04-07 09:11:37 +0800

    add tmux

--- scripts/install/conf.sh
+++ scripts/install/conf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 [ -f ~/.zshrc ] || ln -s $PWD/zshrc ~/.zshrc
 [ -f ~/.slate ] || ln -s $PWD/slate.conf ~/.slate
+[ -f ~/.tmux.conf ] || ln -s $PWD/tmux.conf ~/.tmux.conf
---
+++ scripts/install/tmux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+which tmux >/dev/null 2>&1 && exit
+brew install tmux
