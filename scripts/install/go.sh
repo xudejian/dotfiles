d3b6307c74dd77fbcb2014c8f9c30b3327120356
Dejian Xu <xudejian2008@gmail.com>
2015-11-09 09:34:56 +0800

    install godoc

--- scripts/install/go.sh
+++ scripts/install/go.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-which go >/dev/null 2>&1 || brew install go
+which go >/dev/null 2>&1 && exit
+brew install go
+[ -f ~/Library/LaunchAgents/godoc.plist ] || ln -s $PWD/godoc.plist ~/Library/LaunchAgents
