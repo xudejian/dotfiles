8101dde8c1f8f7f3d35143db429334486eb60c2f
Dejian Xu <xudejian2008@gmail.com>
2016-08-29 13:29:51 +0800

    update install

--- scripts/install/brew.sh
+++ scripts/install/brew.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 which brew >/dev/null 2>&1 && exit
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask
--- scripts/install/coffee.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#require node npm
-which coffee >/dev/null 2>&1 && exit
-npm install -g coffee-script
