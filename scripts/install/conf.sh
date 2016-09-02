76370acffa774b2734dbfb6d8594e9aedb97d0d0
Dejian Xu <xudejian2008@gmail.com>
2016-09-02 10:35:58 +0800

    for ssh config

--- scripts/install/conf.sh
+++ scripts/install/conf.sh
@@ -4,3 +4,8 @@
 [ -f ~/.tmux.conf ] || ln -s $PWD/tmux.conf ~/.tmux.conf
 [ -f ~/.ctags ] || ln -s $PWD/ctags.conf ~/.ctags
 [ -f ~/.editorconfig ] || ln -s $PWD/.editorconfig ~/.editorconfig
+
+if [ ! -f ~/.ssh/config ]; then
+	mkdir -p ~/.ssh
+	cp $PWD/ssh_config ~/.ssh/config
+fi
